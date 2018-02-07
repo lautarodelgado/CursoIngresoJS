5101ed41396a96bb1b399d0dec5c9b04a8ca200e
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-07 10:21:20 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	var importe;
+	var descuento;
+	var resultado; 
+
+	importe = document.getElementById('importe').value;
+
+	importe = parseInt(importe);
+
+	descuento = importe*0.25;
+
+	resultado= importe - descuento;
+
+	document.getElementById('resultado').value = resultado;
+
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,21 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo;
+	var resultado;
+	var aumento;
+
+ 	sueldo = document.getElementById('sueldo').value;
+
+	sueldo = parseInt(sueldo);
+
+	aumento = sueldo*10/100;
+	aumento = parseInt(aumento);
+	resultado = sueldo + aumento;
+
+	document.getElementById('resultado').value = resultado;
+
+
 
 	
 }
