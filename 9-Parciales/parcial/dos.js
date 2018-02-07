73d05b8404bc1418108625ee9397737726bfea90
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-07 11:46:56 -0300

    Add files via upload

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,19 @@
 function Mostrar()
 {
-  
+  var importe;
+  var iva;
+  var importeFinal;
+
+
+
+  importe = prompt("Importe:");
+
+  importe = parseInt(importe);
+  iva = importe*0.21;
+
+  importeFinal = importe + iva;
+
+
+  document.getElementById('importeFinal').value = importeFinal;
+
 }
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,19 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var perimetroPorTresHilos;
+
+	largo = document.getElementById('alrgo').value;
+	ancho = document.getElementById('ancho').value;
+
+	perimetro = largo*ancho;
+	perimetroPorTresHilos = perimetro*3;
+
+	alert("Se necesitan " + perimetroPorTresHilos + " metros de alambre.");
+
+
+
 
 }
