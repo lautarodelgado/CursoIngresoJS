4ee8383b81ac8293250722db24e1b3fb5e17a231
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-08 21:22:28 -0300

    Add files via upload
    2 "if" y 2 "else"

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,39 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+
+	numeroUno = prompt("Ingrese un número:");
+	numeroDos = prompt("Ingrese otro número:");
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	if(numeroUno == numeroDos)
+	{
+		alert(numeroUno*numeroDos);
+	}
+
+		else
+		{
+			if(numeroUno > numeroDos)
+			{
+			alert(numeroUno - numeroDos);
+			}
+			else
+			{
+				alert(numeroUno + numeroDos);
+			}
+		}
+
+			
+
+
+
+
+
+
+
+
 
 }
