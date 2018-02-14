71531ce2fc3757cbc9a362484f8380f7a3348f7e
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-14 09:04:38 -0300

    Hecho

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -5,21 +5,21 @@ function Mostrar()
 
 	edad = document.getElementById('edad').value;
 
-	if(edad > 17)
+	if(edad > 17)   //si el if es de una sola linea puede no tener llaves, porque ejecuta una sola linea; lo mismo pasa con el else
 	{
 		alert("Es mayor de edad.");
 	}
+	else
+	{
+		if(edad > 12 && edad <18)
+		{
+			alert("Es adolescente.");
+		}
 		else
 		{
-			if(edad > 12 && edad <18)
-			{
-				alert("Es adolescente.");
-			}
-				else
-				{
-						alert("Es un niño.");
-				}
+			alert("Es un niño.");
 		}
+	}
 
 
 
