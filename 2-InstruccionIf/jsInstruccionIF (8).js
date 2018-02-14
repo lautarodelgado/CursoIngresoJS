23ff1af10ef0da925cb48b90fd64bac5c5cc397c
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-14 09:42:53 -0300

    Hecho con cosole.log

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -4,12 +4,18 @@ function Mostrar()
 	var edad;
 	var estadoCivil;
 
+	//console.log("hola"); --> solamente se va a ver en la consola (F12)
+
 	edad = document.getElementById('edad').value;
 	estadoCivil = document.getElementById('estadoCivil').value;
 
 	if(estadoCivil == "Soltero" && edad > 17)
 	{
 		alert("Es soltero y no es menor.");
+	}
+	else
+	{
+		console.log("NO HACER NADA");
 	}
 
 
