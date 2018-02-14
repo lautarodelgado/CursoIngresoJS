13c54e52396dd732bea235282f1ea0e75a27406a
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-14 18:49:47 -0300

    Hecho

--- 9-Parciales/parcial/cinco.js
+++ 9-Parciales/parcial/cinco.js
@@ -1,4 +1,21 @@
 function Mostrar()
 {
+	var diaDeLaSemana;
 
+	diaDeLaSemana = prompt("Ingrese un día de la semana");
+
+	switch(diaDeLaSemana)
+	{
+		case "Sábado":
+		case "Domingo":
+			alert("es fin de semana");
+			break;
+
+		case "Lunes":
+		case "Martes":
+		case "Miércoles":
+		case "Jueves":
+		case "Viernes":
+			alert("a trabajar !!!");
+	}
 }
