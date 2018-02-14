dda4cc82851d07244b95e2bd24f4f455f7785a6f
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-14 18:55:49 -0300

    Hecho

--- 9-Parciales/parcial2/5-switch.js
+++ 9-Parciales/parcial2/5-switch.js
@@ -1,7 +1,29 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 5-Switch");
-	
+	var mes;
+
+	mes = prompt("Ingrese un mes:");
+
+	switch(mes)
+	{
+		case "enero":
+		case "febrero":
+			alert("Veranito!!!!");
+			break;
+
+		case "marzo":
+		case "abril":
+		case "abril":
+		case "mayo":
+		case "junio":
+		case "julio":
+		case "agosto":
+		case "septiembre":
+		case "octubre":
+		case "noviembre":
+		case "diciembre":
+			alert("extraño enero y febrero!!!");
+	}
 }
 
