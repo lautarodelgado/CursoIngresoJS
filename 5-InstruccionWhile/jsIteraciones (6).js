a99296a7ac361935c30fcafc0de8aa317b269f4d
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-20 11:13:33 -0300

    hecho por el profesor

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,19 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+	var promedio;
 
+	while(contador < 5)
+	{
+		contador++;
+		numero = prompt("Ingrese un número:");
+		numero = parseInt(numero);
+		acumulador = acumulador+numero;
+	}
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	promedio = acumulador/contador;
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
