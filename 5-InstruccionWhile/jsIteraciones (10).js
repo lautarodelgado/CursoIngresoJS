f9eb9f7ede3c5d1c4b6e0eeec1fac8df4a113393
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-20 18:34:56 -0300

    Incompleto

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,17 +2,55 @@ function Mostrar()
 {
 
 	var contador=0;
-	//declarar contadores y variables 
-	
+	var negativos=0;
+	var positivos=0;	
 	var respuesta="si";
+	var numero;
+	var cantidadPositivos=0;
+	var cantidadNegativos=0;
+	var cantidadCeros;
+	var cantidadPares;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferenciaPositivosNegativos;
 
-	while(respuesta!="no")
+	while(respuesta == "si")
 	{
+		contador++
+		numero = prompt("Ingrese un numero");
+		numero = parseInt(numero);
+		respuesta = prompt("Escriba si para continuar");
+
+
+		if(numero < 0)
+		{
+			negativos = negativos + numero;
+		}
+		else
+		{
+			if(numero > 0)
+			{
+				positivos = positivos + numero;
+			}
+			else
+			{
+				if(numero > 0)
+				{
+					cantidadPositivos = cantidadPositivos + contador;
+				}
+			}
+
+			if(numero < 0)
+				{
+					cantidadNegativos = cantidadNegativos + contador;
+				}
+			
+		}
+
 		
 	
 	}
 
-
-
+	document.write("<br>" + negativos + "<br>" + positivos + "<br>" + cantidadPositivos + "<br>" + cantidadNegativos);
 
 }//FIN DE LA FUNCIÓN
