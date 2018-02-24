569b2f73412cbf73596f9f8889ed65c1091e4dbf
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-24 10:32:16 -0300

    hecho con validacion

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -2,15 +2,33 @@
 
 function Mostrar()
 {
+	var alumno;
+	var nota;
+	var notaSuma=0;
+	var promedio;
 	var contador=0;
 	var respuesta = "si"; // o se puede poner "" y en el while != "no"
-	var alumno;
-
-	while(respuesta == "si")
+	var promedio;
+	
+	while(respuesta =="si")
 	{
 		contador++;
 		alumno = prompt("Ingrese el nombre de un alumno:");
+		nota = prompt("Ingrese la nota del alumno");
+		nota = parseInt(nota);
+		while(nota < 0 || nota >10)      //validacion
+		{
+			nota = prompt("Ingrese la nota del alumno");
+			nota = parseInt(nota);
+		}
+
+		notaSuma = notaSuma + nota;
+
 		respuesta = prompt("Ingrese si para continuar");
 	}
-	document.write(contador);
+
+	promedio = notaSuma/contador;
+
+	document.write("La cantidad de alumnos es " + contador);
+	document.write("<br> El promedio de las notas es " + promedio);
 }
