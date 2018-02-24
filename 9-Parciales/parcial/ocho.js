c08d47b3b3f75fbcfea5979d1eef769f5e50eccd
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-02-24 09:58:20 -0300

    Add files via upload

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,4 +1,16 @@
+// hacer un ingreso de alumno, no sabemos cuantos alumnos hay, pedimos el nombre. Hay que mostrar cuàntos alumnos ingrese(document.write)
+
 function Mostrar()
 {
+	var contador=0;
+	var respuesta = "si"; // o se puede poner "" y en el while != "no"
+	var alumno;
 
+	while(respuesta == "si")
+	{
+		contador++;
+		alumno = prompt("Ingrese el nombre de un alumno:");
+		respuesta = prompt("Ingrese si para continuar");
+	}
+	document.write(contador);
 }
